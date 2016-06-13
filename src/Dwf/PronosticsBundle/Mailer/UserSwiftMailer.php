43fef9c14078a0b15868e0e3e5a5743564316341
michelcourtade <michel.courtade@gmail.com>
2016-06-13 15:57:01 +0200

    Add Lost Password functionnality (with trans)

--- src/Dwf/PronosticsBundle/Mailer/UserSwiftMailer.php
+++ src/Dwf/PronosticsBundle/Mailer/UserSwiftMailer.php
@@ -3,6 +3,7 @@ namespace Dwf\PronosticsBundle\Mailer;
 
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use FOS\UserBundle\Model\UserInterface;
 use FOS\UserBundle\Mailer\MailerInterface;
 use FOS\UserBundle\Mailer\TwigSwiftMailer;
@@ -16,17 +17,17 @@ class UserSwiftMailer extends TwigSwiftMailer implements MailerInterface
         $app = $this->twig->getGlobals();
         $template = $this->parameters['template']['invitation'];
         $url = $this->router->generate('fos_user_registration_register', array('invitation' => $invitation->getCode(), 'email' => $invitation->getEmail()), true);
-    
+
         $context = array(
                 'user'              => $user,
                 'confirmationUrl'   => $url,
                 'invitation_code'   => $invitation->getCode(),
                 'app_name'          => $app["app_name"]
         );
-    
+
         $this->sendMessage($template, $context, $this->parameters['from_email']['invitation'], $invitation->getEmail());
     }
-    
+
     public function getName() {
         return 'user_swift_mailer';
     }
