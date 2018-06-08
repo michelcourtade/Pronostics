35c2d1264859ce641261448d8562f887666d31bc
Michel Courtade <michel.courtade@gmail.com>
2018-06-08 23:12:50 +0200

    Fix register with block prefix

--- src/Dwf/PronosticsBundle/Form/Type/RegistrationFormType.php
+++ src/Dwf/PronosticsBundle/Form/Type/RegistrationFormType.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Dwf\PronosticsBundle\Form\Type;
 
 use FOS\UserBundle\Form\Type\RegistrationFormType as BaseRegistrationFormType;
@@ -6,21 +7,41 @@ use Symfony\Component\Form\FormBuilderInterface;
 
 class RegistrationFormType extends BaseRegistrationFormType
 {
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         parent::buildForm($builder, $options);
-        $builder->add('invitation', 'dwf_pronosticsbundle_invitation_type', array(
-                                                                                'required' => false
-        ));
+        $builder->add('invitation', 'dwf_pronosticsbundle_invitation_type',
+            array(
+                'required' => false
+            )
+        );
     }
-    
+
+    /**
+     * @return null|string|\Symfony\Component\Form\FormTypeInterface
+     */
     public function getParent()
     {
         return 'fos_user_registration';
     }
 
+    /**
+     * @return null|string
+     */
     public function getName()
     {
         return 'dwf_pronosticsbundle_user_registration';
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getBlockPrefix()
+    {
+        return 'dwf_fos_user_registration';
     }
 }
