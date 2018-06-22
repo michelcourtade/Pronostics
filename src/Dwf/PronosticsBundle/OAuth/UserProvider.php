0f77a824cb3c4687e739800b23b50ab435234327
Michel Courtade <michel.courtade@gmail.com>
2018-06-22 22:49:08 +0200

    Fix facebook profile pic

--- src/Dwf/PronosticsBundle/OAuth/UserProvider.php
+++ src/Dwf/PronosticsBundle/OAuth/UserProvider.php
@@ -2,21 +2,10 @@
 
 namespace Dwf\PronosticsBundle\OAuth;
 
-use FOS\UserBundle\Model\UserInterface as FOSUserInterface;
-
 use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
 use HWI\Bundle\OAuthBundle\Security\Core\User\FOSUBUserProvider;
-
-use Dwf\PronosticsBundle\Entity\User;
-
-use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-/**
- * Loading and ad-hoc creation of a user by an OAuth sign-in provider account.
- *
- * @author Fabian Kiss <fabian.kiss@ymc.ch>
- */
 class UserProvider extends FOSUBUserProvider
 {
     /**
@@ -26,11 +15,11 @@ class UserProvider extends FOSUBUserProvider
     {
         $property = $this->getProperty($response);
         $username = $response->getUsername();
-var_dump("connect");exit();
+
         //on connect - get the access token and the user ID
-        $service = $response->getResourceOwner()->getName();
-        $setter = 'set'.ucfirst($service);
-        $setter_id = $setter.'Id';
+        $service      = $response->getResourceOwner()->getName();
+        $setter       = 'set'.ucfirst($service);
+        $setter_id    = $setter.'Id';
         $setter_token = $setter.'AccessToken';
         //we "disconnect" previously connected users
         if (null !== $previousUser = $this->userManager->findUserBy(array($property => $username))) {
@@ -52,9 +41,8 @@ var_dump("connect");exit();
     {
         $username = $response->getUsername();
         $nickname = $response->getNickname();
-        $email = $response->getEmail();
-        var_dump("load");exit();
-        //$user = $this->userManager->findUserBy(array($this->getProperty($response) => $username));
+        $email    = $response->getEmail();
+
         $user = $this->userManager->findUserBy(array('email' => $email));
         //when the user is registrating
         if (null === $user) {
@@ -62,21 +50,22 @@ var_dump("connect");exit();
             $setter = 'set'.ucfirst($service);
             $setter_id = $setter.'Id';
             $setter_token = $setter.'AccessToken';
-            // create new user here
+
             $user = $this->userManager->createUser();
             $user->$setter_id($username);
             $user->$setter_token($response->getAccessToken());
-            //I have set all requested data with the user's username
-            //modify here with relevant data
+
             $user->setUsername($nickname);
-            if($email)
+            $user->setEmail($username);
+            if($email) {
                 $user->setEmail($email);
-            else $user->setEmail($username);
+            }
 
             if ($response->getProfilePicture()) {
-                $dest = 'web/uploads/'.$username.'.jpg';
+                $img  = 'uploads/documents/'.$username.'.jpg';
+                $dest = dirname(__FILE__). '/../../../../web/'.$img;
                 copy($response->getProfilePicture(), $dest);
-                $user->setProfilePicture($dest);
+                $user->setProfilePicture($img);
             }
             if(!$user->getPassword()) {
                 // generate unique token
@@ -85,18 +74,28 @@ var_dump("connect");exit();
             }
             $user->setEnabled(true);
             $this->userManager->updateUser($user);
+
             return $user;
         }
         //if user exists - go with the HWIOAuth way
         $serviceName = $response->getResourceOwner()->getName();
         $setterId = 'set' . ucfirst($serviceName) . 'Id';
         $user->$setterId($response->getUsername());
-        $user->setProfilePicture($response->getProfilePicture());
+
+        if ($response->getProfilePicture()) {
+            $img  = 'uploads/documents/'.$username.'.jpg';
+            $dest = dirname(__FILE__). '/../../../../web/'.$img;
+            copy($response->getProfilePicture(), $dest);
+            $user->setProfilePicture($img);
+        }
+
         $this->userManager->updateUser($user);
+
         $user = parent::loadUserByOAuthUserResponse($response);
+
         $setterToken = 'set' . ucfirst($serviceName) . 'AccessToken';
-        //update access token
         $user->$setterToken($response->getAccessToken());
+
         return $user;
     }
 }
